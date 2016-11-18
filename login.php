f3ff978cbca0121d9b2919aafeb82f7afba9857b
meyu <m978718484@163.com>
2016-11-18 20:08:57 +0800

    get data

--- login.php
+++ login.php
@@ -11,7 +11,6 @@
             <form action="index.php" method="post">     
                 用户名：<input type="text" name="username">                 
                 密码：<input type="password" name="password">                 
-                <!-- <input type="submit" value="登陆">   -->
                 <a data-role="button" href="javascript:location.href='index.php'" data-ajax="false" id = "btnLogin">登陆</a>     
             </form>
         </div>
