6fb5954c2b5fe59ff8c850051a690b7f0f07fe62
m978718484 <m978718484@163.com>
2016-12-06 22:58:12 +0800

    add redis extension

--- common.php
+++ common.php
@@ -11,14 +11,12 @@
         case 'getDetail':
             getDetail();
           break;
-        default:
-          break;
     }
 
     function getLogin()
     {
     	$url = 'http://10.138.1.17:8082/EbidServices/checkLogin?userName=ebid&password=3333302eac36ff5158dc5d3ff2db2447';
-       	getJson($url);
+      getJson($url);
     }
 
     function getList()
@@ -31,20 +29,20 @@
     function getDetail()
     {
     	$bidno = $_REQUEST["bidno"];
-        $url = "http://10.138.1.17:8082/EbidServices/getBid?bidno=".$bidno."&token=P1e8UbJZmtQUQ8LtOg98ff0LjmHivcPiK50aLPNv0AU";
-       	getJson($url);
+      $url = "http://10.138.1.17:8082/EbidServices/getBid?bidno=".$bidno."&token=P1e8UbJZmtQUQ8LtOg98ff0LjmHivcPiK50aLPNv0AU";
+      getJson($url);
     }
 
-	function getJson($url){
-		$handle = fopen($url,"rb");
-		$content = "";
-		while (!feof($handle)) {
-		    $content .= fread($handle, 10000);
-		}
-		fclose($handle);
-		$content = json_encode($content);
-		echo $content;
-	}
+  	function getJson($url){
+  		$handle = fopen($url,"rb");
+  		$content = "";
+  		while (!feof($handle)) {
+  		  $content .= fread($handle, 10000);
+  		}
+  		fclose($handle);
+  		$content = json_encode($content);
+  		echo $content;
+  	}
 
 
 ?>
---
+++ package/README.txt
@@ -0,0 +1,3 @@
+ÅäÖÃÕfÃ÷£ºhttp://ipple.net/lanmp/win32-redis-php5.html
+
+ÔOÖÃºÃááÖØ†¢Apache
